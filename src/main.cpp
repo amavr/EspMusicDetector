abdc4e4aad870d205a48ba7dd21d6b4759732e50
amavr <albert.mavrin@gmail.com>
2023-12-26 03:34:45 +0300

    Подбор коэф.нормализации

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,16 @@
 #define SAMPLING_FREQ 40000     // Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT Fmax=sampleF/2.
 #define AUDIO_IN_PIN GPIO_NUM_9 // Signal in on this pin
 
-#define NUM_BANDS 8 // To change this, you will need to change the bunch of if statements describing the mapping from bins to bands
-#define NOISE 20000   // Used as a crude noise filter, values below this are ignored
+#define NOISE 4000 // Used as a crude noise filter, values below this are ignored
 
 // Sampling and FFT stuff
 unsigned int sampling_period_us;
-byte peak[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // The length of these arrays must be >= NUM_BANDS
-int bandValues[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-const uint8_t BAND_NUM = 8;
-int steps[BAND_NUM] = {3, 6, 13, 27, 55, 112, 229, 256};
-long bands[BAND_NUM] = {0, 0, 0, 0, 0, 0, 0, 0};
+const uint8_t BAND_NUM = 3;
+int steps[BAND_NUM] = {5, 96, 256};
+long bands[BAND_NUM] = {0, 0, 0};
+long avgMax[BAND_NUM] = {0, 0, 0};
+long noises[BAND_NUM] = {32000, 20000, 4000};
 
 double vReal[SAMPLES];
 double vImag[SAMPLES];
@@ -29,14 +28,12 @@ void setup()
     sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQ));
 }
 
-long avgMax = 0;
-
 void loop()
 {
     // Reset bandValues[]
-    for (int i = 0; i < NUM_BANDS; i++)
+    for (int i = 0; i < BAND_NUM; i++)
     {
-        bandValues[i] = 0;
+        bands[i] = 0;
     }
 
     // Sample the audio pin
@@ -50,51 +47,55 @@ void loop()
         }
     }
 
-    // ВЫЧМСЛЕНИЕ БПФ
+    // ВЫЧИСЛЕНИЕ БПФ
     FFT.DCRemoval();
     FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
     FFT.Compute(FFT_FORWARD);
     FFT.ComplexToMagnitude();
 
     // ГРУППИРОВКА ДАННЫХ ПО ПОЛОСАМ
-    int step_prev = 2;
-    long curMax = NOISE * 2;
-    for (int s = 0; s < BAND_NUM; s++)
+    long curMax = NOISE << 4;
+    for (int i = 2; i < SAMPLES / 2; i++)
     {
-        bands[s] = 0;
-        int step = steps[s] - step_prev;
-        for (int i = step_prev; i < steps[s]; i++)
+        if (i <= 4 && vReal[i] > noises[0])
         {
-            if (vReal[i] > NOISE)
-            {
-                bands[s] += (long) vReal[i];
-            }
+            bands[0] += (long)vReal[i];
         }
-        bands[s] = bands[s] / step;
-        step_prev = steps[s];
-
-        if(bands[s] > curMax) curMax = (long) bands[s];
+        else if (i > 7 && i < 150 && vReal[i] > noises[1])
+        {
+            bands[1] += (long)vReal[i];
+        }
+        else if (i > 200 && vReal[i] > noises[2])
+        {
+            bands[2] += (long)vReal[i];
+        }
     }
-
-    // УСТАНОВКА ЧУВСТВИТЕЛЬНОСТИ ЧЕРЕЗ УСРЕД.МАКС.ЗНАЧЕНИЯ
-    // адаптивный коэф.
-    uint8_t k = abs(curMax - avgMax) > 1000 ? 3 : 4;
-    // бегущее среднее * 2^(k + 1)
-    avgMax += ((curMax << (k + 1)) - avgMax) >> k;
-    // восстановленное бегущее среднее
-    long maxVal = avgMax >> (k + 1);
-
-    // МАСШТАБИРОВАНИЕ к 0..255
-    for (int s = 0; s < BAND_NUM; s++)
-    {
-        bands[s] = map(constrain(bands[s], NOISE, maxVal), NOISE, maxVal, 0, 255);
-    }
-
 
     Serial.println("loop");
 
-    Serial.print(">max:");
-    Serial.println(maxVal);
+    // Serial.print(">max:");
+    // Serial.println(maxVal / 1000);
+    // Serial.printf(">0:%d\n", bands[0] / 1000);
+    // Serial.printf(">1:%d\n", bands[1] / 1000);
+    // Serial.printf(">2:%d\n", bands[2] / 1000);
+
+    // МАСШТАБИРОВАНИЕ к 0..255
+    for (int i = 0; i < BAND_NUM; i++)
+    {
+        // УСТАНОВКА ЧУВСТВИТЕЛЬНОСТИ ЧЕРЕЗ УСРЕД.МАКС.ЗНАЧЕНИЯ
+        curMax = max(noises[i], bands[i]);
+        // адаптивный коэф.
+        uint8_t k = abs(curMax - avgMax[i]) > 10000 ? 2 : 4;
+        // бегущее среднее * 2^(k + 1)
+        avgMax[i] += ((curMax << (k + 1)) - avgMax[i]) >> k;
+        // восстановленное бегущее среднее
+        long maxVal = (avgMax[i] >> (k + 1)) * 3;
+
+        bands[i] = map(constrain(bands[i], 0, maxVal), 0, maxVal, 0, 255);
+    }
+
+    // Serial.print(">max:");
+    // Serial.println(maxVal / 1000);
     Serial.printf(">0:%d\n", bands[0]);
     Serial.printf(">1:%d\n", bands[1]);
     Serial.printf(">2:%d\n", bands[2]);
